9a8b296e5bf04ee9af9bfb316d4cbec254fd4857
AngelaZou2000 <az292@cornell.edu>
2020-03-05 01:19:43 -0500

    test mulFN

---
+++ sim/HardFloat/mulRecFNRTL.py
@@ -0,0 +1,41 @@
+#=========================================================================
+# Wrapper for HardFloat's addition and subtraction module
+
+#=========================================================================
+
+from pymtl3 import *
+from pymtl3.passes.backends.verilog import \
+    VerilogPlaceholderConfigs, VerilatorImportConfigs, TranslationConfigs
+
+class mulRecFNRTL( Placeholder, Component ):
+
+  # Constructor
+
+  def construct( s, expWidth = 3, sigWidth = 3 ):
+
+    # Interface
+	
+    s.control        = InPort ()
+    s.subOp          = InPort ()
+    s.a              = InPort ( mk_bits(expWidth + sigWidth + 1) )
+    s.b              = InPort ( mk_bits(expWidth + sigWidth + 1) )
+    s.roundingMode   = InPort ( mk_bits(3) ) 
+    
+    s.out            = OutPort ( mk_bits(expWidth + sigWidth + 1) )
+    s.exceptionFlags = OutPort ( mk_bits(5) )
+
+    # Configurations
+
+    from os import path
+    s.config_placeholder = VerilogPlaceholderConfigs(
+      src_file   = path.dirname(__file__) + '/source/mulRecFN.v',
+      #v_include  = [path.dirname(__file__) + '/source/RISCV'],
+      top_module = 'mulRecFN',
+      has_clk    = False,
+      has_reset  = False,
+    )
+
+    s.config_verilog_import = VerilatorImportConfigs(
+      vl_Wno_list = ['WIDTH', 'MODDUP'], # to surpress some Verilator warnings
+    )
+
---
+++ sim/HardFloat/test/mulRecFNRTL_test.py
@@ -0,0 +1,19 @@
+#=========================================================================
+# mulRecFNRTL_test.py
+#=========================================================================
+
+from pymtl3 import *
+from pymtl3.stdlib.test import run_test_vector_sim
+from HardFloat.mulRecFNRTL import mulRecFNRTL 
+ 
+
+def test_simple( dump_vcd, test_verilog ):
+  expWidth = 8
+  sigWidth = 8
+  run_test_vector_sim( mulRecFNRTL(expWidth = expWidth, sigWidth = sigWidth), [
+    ('subOp	 a  b  roundingMode  out*  exceptionFlags*'), 
+		[ b1(0), mk_bits(expWidth + sigWidth + 1)(0), mk_bits(expWidth + sigWidth + 1)(0), \
+		b3(0), mk_bits(expWidth + sigWidth + 1)(0), '?'],  
+  ], dump_vcd, test_verilog )
+  
+
