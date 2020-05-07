812696ca909fd0e5b6052a51966d799330baadfe
Shunning Jiang <sj634@cornell.edu>
2020-05-06 22:27:14 -0400

    add new converter function

--- HardFloat/test/ConverterFuncs_test.py
+++ HardFloat/test/ConverterFuncs_test.py
@@ -10,330 +10,264 @@ from pymtl3.datatypes import strategies as pm_st
 
 import hypothesis
 from hypothesis import given
+from hypothesis import settings
 from hypothesis import strategies as st
 
 from HardFloat.converter_funcs import floatToFN, fNToFloat
 
+import math
 import random
 
 # ========================== Helper functions ============================
 def get_rand( low, high, precision ):
   val = round(random.uniform(low, high), precision)
-  while abs_val(val) < 1:
+  while abs(val) < 1:
     val = round(random.uniform(low, high), precision)
 
-def abs_val( x ):
-  if(x < 0):
-    return -x
-  else:
-    return x
 # ========================================================================
 
 # =================== Half-precision directed tests ======================
-precision = 16
-tolerance = 0.00001
 
 def test_addF16_positive():
+  precision = 16
+  tolerance = 0.01
 
   in_float  = 17.29
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF16_negative():
+  precision = 16
+  tolerance = 0.01
 
   in_float  = -18.49
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF16_abs_less_than_one():
+  precision = 16
+  tolerance = 0.01
 
   in_float  = 0.52
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
   in_float  = -0.12
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF16_trailing_zeroes():
+  precision = 16
+  tolerance = 0.01
 
   in_float  = 12.008
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF16_integer():
+  precision = 16
+  tolerance = 0.01
 
   in_float  = 24
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
   in_float  = -58.0
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
+  assert abs(in_float - out_float) < tolerance
 
-  assert(abs_diff < tolerance)
 # ========================================================================
 
 # ================== Single-precision directed tests =====================
-precision = 32
-tolerance = 0.00000001
 
 def test_addF32_positive():
+  precision = 32
+  tolerance = 0.000001
 
   in_float  = 182.94011
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF32_negative():
+  precision = 32
+  tolerance = 0.00001
 
   in_float  = -192.491
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF32_abs_less_than_one():
+  precision = 32
+  tolerance = 0.000001
 
   in_float  = 0.29481
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
   in_float  = -0.511003
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF32_trailing_zeroes():
+  precision = 32
+  tolerance = 0.0001
 
   in_float  = 1289.00019
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF32_integer():
+  precision = 32
+  tolerance = 0.000001
 
   in_float  = 5819
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
   in_float  = -9930.0
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
+  assert abs(in_float - out_float) < tolerance
 
-  assert(abs_diff < tolerance)
 # ========================================================================
 
 # ================== Double-precision directed tests =====================
-precision = 64
-tolerance = 0.000001
 
 def test_addF64_positive():
+  precision = 64
+  tolerance = 0.00000001
 
   in_float  = 27367198.201812
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF64_negative():
+  precision = 64
+  tolerance = 0.00000001
 
   in_float  = -18884.101
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF64_abs_less_than_one():
+  precision = 64
+  tolerance = 0.00000001
 
   in_float  = 0.299994812
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
   in_float  = -0.2300001
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF64_trailing_zeroes():
+  precision = 64
+  tolerance = 0.00000001
 
   in_float  = 3919.0000192
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
 def test_addF64_integer():
+  precision = 64
+  tolerance = 0.00000001
 
   in_float  = 481331.0
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
+  assert abs(in_float - out_float) < tolerance
 
   in_float  = -5818283
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+  in_rec    = floatToFN(in_float, precision=precision)
+  out_float = fNToFloat(in_rec, precision=precision)
 
-  abs_diff = abs_val(in_float - out_float)
+  assert abs(in_float - out_float) < tolerance
 
-  assert(abs_diff < tolerance)
-# ========================================================================
+# ================= hypothesis testing ====================
 
-# ================= Half-precision hypothesis testing ====================
-precision = 16
+@settings(max_examples=1000)
+@given( in_float = st.floats(width=16, allow_infinity=True, allow_nan=True) )
+def test_hypothesis_16( in_float ):
 
-@given( in_float = st.floats(min_value = -10000.0, max_value = 10000.0) )
-def test_hypothesis_F16( in_float ):
+  in_rec    = floatToFN(in_float, precision=16)
+  out_float = fNToFloat(in_rec, precision=16)
+  print(in_float, hex(in_rec), out_float)
 
-  tolerance = 0.0000001
+  if math.isnan(in_float):  assert math.isnan(out_float)
+  else:                     assert in_float == out_float
 
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+# sigh, native python log2 cannot handle values that are close to max
+@settings(max_examples=1000)
+@given( in_float = st.floats(width=32, allow_infinity=True, allow_nan=True) )
+def test_hypothesis_32( in_float ):
 
-  abs_diff = abs_val(in_float - out_float)
+  in_rec    = floatToFN(in_float, precision=32)
+  out_float = fNToFloat(in_rec, precision=32)
+  print(in_float, hex(in_rec), out_float)
 
-  assert(abs_diff < tolerance)
-# ========================================================================
+  if math.isnan(in_float):  assert math.isnan(out_float)
+  else:                     assert in_float == out_float
 
-# ================ Single-precision hypothesis testing ===================
-precision = 32
+@settings(max_examples=1000)
+@given( in_float = st.floats(width=64, min_value=-1e20, max_value=1e20,allow_infinity=False, allow_nan=False) )
+def test_hypothesis_64_small( in_float ):
 
-@given( in_float = st.floats(width = precision, allow_infinity=False, allow_nan=False) )
-def test_hypothesis_F32( in_float ):
+  in_rec    = floatToFN(in_float, precision=64)
+  out_float = fNToFloat(in_rec, precision=64)
+  print(in_float, hex(in_rec), out_float)
 
-  tolerance = 0.0000000001
+  assert in_float == out_float
 
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
+@settings(max_examples=1000)
+@given( in_float = st.floats(width=64, min_value=-1.79E308, max_value=1.79E308,allow_infinity=False, allow_nan=False) )
+def test_hypothesis_64_big( in_float ):
 
-  abs_diff = abs_val(in_float - out_float)
+  in_rec    = floatToFN(in_float, precision=64)
+  out_float = fNToFloat(in_rec, precision=64)
+  print(in_float, hex(in_rec), out_float)
 
-  assert(abs_diff < tolerance)
-# ========================================================================
-
-# ================ Single-precision hypothesis testing ===================
-precision = 64
-
-@given( in_float = st.floats(width = precision, allow_infinity=False, allow_nan=False) )
-def test_hypothesis_F64( in_float ):
-
-  tolerance = 0.000000000000000000001
-
-  in_rec    = floatToFN(in_float, precision)
-  out_float = fNToFloat(in_rec, precision)
-
-  abs_diff = abs_val(in_float - out_float)
-
-  assert(abs_diff < tolerance)
-# ========================================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if math.isnan(in_float):
+    assert math.isnan(out_float)
+  elif in_float == float("inf") or in_float == float("-inf"):
+    assert out_float == in_float
+  else:
+    assert abs(in_float - out_float) <= abs((1e-10)*in_float)
