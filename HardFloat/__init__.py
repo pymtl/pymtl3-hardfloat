5839d18c2abd561d0530f93f3bfceedbe4658d8b
Shunning Jiang <sj634@cornell.edu>
2020-07-25 22:40:12 -0400

    add module level import

--- HardFloat/__init__.py
+++ HardFloat/__init__.py
@@ -1,1 +1,8 @@
-
+from .AddFNRTL  import AddFN
+from .AddRecFNRTL  import AddRecFN
+from .FNToRecFNRTL import FNToRecFN
+from .MulAddFNRTL import MulAddFN
+from .MulAddRecFNRTL import MulAddRecFN
+from .MulFNRTL  import MulFN
+from .MulRecFNRTL  import MulRecFN
+from .RecFNToFNRTL import RecFNToFN
